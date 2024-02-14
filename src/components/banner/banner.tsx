77c035b92a1ab643cd7357213c844ac06cc72733
Carlos Picazo Cuesta <carlospc@ext.inditex.com>
2024-02-14 01:32:13 +0100

    Updates

--- src/components/banner/banner.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './banner.scss';
-
-const Banner = ({ text, fadeOut = false, className = "" }) => {
-  return <h1 className={`fei-banner ${className} ${fadeOut ? "fei-banner--fade-out" : ""}`}>{text}</h1>
-}
-
-export default Banner;
